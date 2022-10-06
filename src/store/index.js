0166794a4d30742810a8e90f92afd1569d711d00
Eduardo <eduardo@conectra.com.br>
2022-10-06 16:11:50 -0300

    feat: adiciona url publica para acoes da api

--- src/store/index.js
+++ src/store/index.js
@@ -29,23 +29,23 @@ export default createStore({
   },
   actions: {
     getTodos({ commit }) {
-      return axios.get('http://localhost:3000/todos')
+      return axios.get('https://my-json-server.typicode.com/eduuk15/ToDo/todos')
         .then((response) => {
           commit('storeTodos', response.data)
         })
     },
     addTodo({ commit }, data) {
-      return axios.post('http://localhost:3000/todos', data).then((response) => {
+      return axios.post('https://my-json-server.typicode.com/eduuk15/ToDo/todos', data).then((response) => {
         commit('storeTodo', response.data);
       })
     },
     updateTodo({ commit }, { id, data}) {
-      return axios.put(`http://localhost:3000/todos/${id}`, data).then((response) => {
+      return axios.put(`https://my-json-server.typicode.com/eduuk15/ToDo/todos/${id}`, data).then((response) => {
       commit('storeTodo', response.data)
       })
     },
     deleteTodo({ commit }, id) {
-      return axios.delete(`http://localhost:3000/todos/${id}`).then(() => {
+      return axios.delete(`https://my-json-server.typicode.com/eduuk15/ToDo/todos/${id}`).then(() => {
         commit('deleteTodo', id)
       })
     }
